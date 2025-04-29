9da838020318af8c5732b059bc60a9c7c0772ebf
Matheus Dayan <mdayan.rocha@gmail.com>
2025-04-29 14:50:08 -0300

    novos arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-import AppRoutes from "./routes/appRoutes"
+import AppRoutes from "./routes/appRoutes";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
         <AppRoutes />
       </div>
     </>
-  )
+  );
 }
 
 export default App
--- src/routes/appRoutes.jsx
+++ src/routes/appRoutes.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Link, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Detalhes from "../pages/Detalhes";
 import Favoritos from "../pages/Favoritos";
 
-export default function appRoutes() {
+export default function AppRoutes() {
     return (
     <>
     <BrowserRouter>
@@ -11,11 +11,12 @@ export default function appRoutes() {
         <Link to="/detalhes">Detalhes</Link>
         <Link to="/favoritos">Favoritos</Link>
 
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/detalhes" element={<Detalhes />} />
-            <Route path="/favoritos" element={<Favoritos />} />
-        </Routes>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/detalhes" element={<Detalhes />} />
+        <Route path="/favoritos" element={<Favoritos />} />
+    </Routes>
+    
     </BrowserRouter>
     </>
     );
