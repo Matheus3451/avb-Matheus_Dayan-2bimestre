542206e16207762cd1965582c47b1b833aa71e63
Matheus Dayan <mdayan.rocha@gmail.com>
2025-05-13 14:11:33 -0300

    save2

--- src/pages/Detalhes/index.jsx
+++ src/pages/Detalhes/index.jsx
@@ -1,7 +1,22 @@
+import axios from "axios"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export default function Detalhes() {
-    return (
+  const { id } = useParams();
+  const [post, setPost] = useState(null);
+
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`)
+      .then(res => setPost(res.data));
+  }, [id]);
+
+  if (!post) return <p>Carregando...</p>;
+
+  return (
     <>
-    <h1 className="text-2x1 font-bold">DETALHES</h1>
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
     </>
-    );
-    }
+  );
+}
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,28 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { Link, Links } from "react-router-dom"
+
 export default function Home() {
+    const [posts, setPost] = useState([])
+    
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10")
+            .then((res) => setPost(res.data))
+
+    })
+
+    console.log(posts)
     return (
     <>
-    <h1 className="text-2x1 font-bold">HOME</h1>
+        <div>
+        <h2> lista de Posts </h2>
+        <ul>
+            {posts.map((post) => (
+            <li> key ={post.id}
+                <Link to={`/detalhes/${post.id}`}>{post.title} </Link>
+            </li>
+        ))}</ul>
+        </div>
     </>
     );
     }
